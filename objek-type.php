1da87ed0e8d1c451933b54f2dc7242072a7d6231
SilviFitriawati29 <silvifitriawati29@gmail.com>
2021-09-26 00:49:01 +0700

    Memahami Bagaimana Caranya Menyelesaikan Problem dengan Menggunakan Konsep Inheritance

--- objek-type.php
+++ objek-type.php
@@ -28,7 +28,7 @@ class Produk{
 
 class CetakInfoProduk{
     public function cetak(Produk $produk ) {
-      $str = "{$produk->judul} | {$produk->getLabel()} {($produk->harga)}";
+      $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
       return $str;
     }
 
